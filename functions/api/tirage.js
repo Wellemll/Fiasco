00fd4723964cd546a33acea354b92729b9293cc3
Wellemll <willem2012@live.ca>
2026-03-15 14:06:31 -0400

    Add files via upload

---
+++ functions/api/tirage.js
@@ -0,0 +1,13 @@
+export async function onRequest(context) {
+    // On récupère la liaison vers la base de données définie dans wrangler.toml
+    const { DB } = context.env;
+
+    // Requête pour avoir les 10 derniers tirages
+    const { results } = await DB.prepare(
+        "SELECT * FROM tirages ORDER BY date_tirage DESC LIMIT 10"
+    ).all();
+
+    return new Response(JSON.stringify(results), {
+        headers: { "Content-Type": "application/json" }
+    });
+}
